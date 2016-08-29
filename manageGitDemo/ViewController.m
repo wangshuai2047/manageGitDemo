6ad5e39da53663ec5c688ae8e7a95259ef81f079
wangshuai <1181168948@qq.com>
2016-08-29 16:59:42 +0800

    第二页界面

--- manageGitDemo/ViewController.m
+++ manageGitDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "WSTwoViewController.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake((self.view.frame.size.width - 100)/2, 100, 100, 30);
+    [btn setTitle:@"下一页" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    btn.layer.cornerRadius = 5;
+    btn.layer.masksToBounds = YES;
+    btn.layer.borderWidth = 0.5;
+    btn.layer.borderColor = [UIColor orangeColor].CGColor;
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+-(void)btnClick:(UIButton*)sender
+{
+    WSTwoViewController *twoVc = [[WSTwoViewController alloc]init];
+    [self.navigationController pushViewController:twoVc animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ manageGitDemo/WSTwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  WSTwoViewController.h
+//  manageGitDemo
+//
+//  Created by 王帅 on 16/8/29.
+//  Copyright © 2016年 王帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSTwoViewController : UIViewController
+
+@end
